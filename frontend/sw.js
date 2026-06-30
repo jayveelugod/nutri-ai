655e404dcabf1df67efea7311d4d7a10051074b1
JV <jayveelugod@gmail.com>
2026-06-30 15:12:09 +0800

    add force auto-update and cache clearing for PWA

--- frontend/sw.js
+++ frontend/sw.js
@@ -8,10 +8,26 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('SW: Clearing Old Cache', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
